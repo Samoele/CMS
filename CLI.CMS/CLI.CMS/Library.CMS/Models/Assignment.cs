a6fbc411bb32358ac594f31ec6fe23da1669ba64
Samuel Marcano <samumarcano@gmail.com>
2026-07-12 23:10:43 -0400

    Started ServiceProxy

--- CLI.CMS/CLI.CMS/Library.CMS/Models/Assignment.cs
+++ CLI.CMS/CLI.CMS/Library.CMS/Models/Assignment.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace Library.CMS.Models
@@ -7,18 +8,10 @@ namespace Library.CMS.Models
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
-        public int TotalPoints { get; set;} = 100;
+        public int AvailablePoints { get; set; }
         public DateTime DueDate { get; set; }
-        public List<Submission> Submissions { get; set; } = new List<Submission>();
-    }
-
-    public class Submission
-    {
-        public int StudentId { get; set; }
-        public string Content { get; set;} = string.Empty;
-        public DateTime SubmittedAt { get; set; } = DateTime.UtcNow;
-        public int? Grade {get; set;} //null if not graded, 1 if graded
-        public bool IsGraded => Grade.HasValue;
         
+        // Track completed work
+        public List<Submission> Submissions { get; set; } = new List<Submission>();
     }
 }
--- CLI.CMS/CLI.CMS/Library.CMS/Models/Module.cs
+++ CLI.CMS/CLI.CMS/Library.CMS/Models/Module.cs
@@ -4,9 +4,7 @@ namespace Library.CMS.Models
 {
     public class Module
     {
-        public string Code { get; set; } = string.Empty; // ex: "COP4870"
-        public string Title { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
-        public List<string> Modules { get; set; } = new List<string>();
+        public int Id { get; set; }
+        public List<string> Content { get; set; } = new List<string>();
     }
 }
